ef24ba422b8aac9d5a1dcdb25378a7267087aab3
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2021-03-23 11:17:29 -0300

    Refactoring app on javascript of typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder: string;
+  Icon?: any;
+}
+
+const Input = ({ name, Icon, ...rest }: InputProps): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -24,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef?.current?.value);
   }, []);
 
   useEffect(() => {
